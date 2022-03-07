50ca9e9925ac4462dcbfa5c577609a678c4db9ed
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-03-07 21:55:27 +0200

    BankTransactionAnalyzerSimple - “Keep It Short and Simple” (KISS) and have the application code in one single class

---
+++ chapter_02/src/main/java/lt/debarz/App.java
@@ -0,0 +1,12 @@
+package lt.debarz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
