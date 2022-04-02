d27d25dd590022a376a577c3995fe3dfa76cda22
deividasbardzenis <deividasbarzdenis@yahoo.com>
2022-04-02 19:19:34 +0300

    Added chapter 6 maven module

---
+++ chapter_06/src/main/java/lt/debarz/App.java
@@ -0,0 +1,13 @@
+package lt.debarz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
